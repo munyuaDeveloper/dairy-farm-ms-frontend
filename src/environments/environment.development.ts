45045fcfbd26e0cfb592b2596e8c1c391c80a774
munyuaDeveloper <munyuapeter06@gmail.com>
2023-11-14 09:44:19 +0300

    changed the base url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     // baseApiUrl: 'http://127.0.0.1:5000'
-    baseApiUrl: 'https://munyua.tech'
+    baseApiUrl: 'https://dairy.api.munyua.tech'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    baseApiUrl: 'https://munyua.tech'
+    baseApiUrl: 'https://dairy.api.munyua.tech'
 };
